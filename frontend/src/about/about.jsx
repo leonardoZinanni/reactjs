98ffea8cb1569d211231fa3a8d23b59bf978952e
leonardo <leonardozinanni@hotmail.com>
2018-05-03 11:37:09 -0300

    agora vai

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+export default props => (
+  <div>
+    <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+    <h2>Nossa Historia</h2>
+    <p> Lorem ipsu dolor sit amet..</p>
+  </div>
+)
---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm';
+import TodoList from  './todoList'
+
+const URL ='http//localhost:3003/api/todos'
+
+export default class Todo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {description: '', list: []}
+    this.handleAdd = this.handleAdd.bind(this)
+    this.handChange = this.handChange.bind(this)
+  }
+
+  handChange (e) {
+    this.setState({...this.state, description: e.target.value})
+    }
+    
+
+  handleAdd(){
+    const description = this.state.description
+    axios.post(URL, {description})
+        .then(resp => console.log('funcionou'))
+  }
+ 
+  render(){
+    return (
+      <div>
+       <PageHeader name='Tarefas' small='cadastro'></PageHeader>
+      <TodoForm description={this.state.description} handleAdd={this.handleAdd} handChange={this.handChange}/>
+      <TodoList />
+     </div>   
+    )
+    
+  }
+}
